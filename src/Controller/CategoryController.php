2cd71c2950be8ef470084fb84e070da9639e62ed
Pagot Marion <marion.pagot@gmail.com>
2023-12-01 16:48:38 +0100

    add category quest 10

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CategoryRepository;
+use App\Repository\ProgramRepository;
+
+#[Route('/category', name: 'category_')]
+class CategoryController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        $categories = $categoryRepository->findAll();
+        return $this->render(
+            'category/index.html.twig',
+            [
+            'categories' => $categories,
+            ]
+        );
+    }
+
+    #[Route('/show/{categoryName}', name: 'show')]
+    public function show(string $categoryName, CategoryRepository $categoryRepository, ProgramRepository $programRepository):Response
+    {
+        $category = $categoryRepository->findOneBy(['name' => $categoryName]);
+        if (!$category) {
+            throw $this->createNotFoundException(
+                'No category with categoryName : '.$categoryName.' found.'
+            );
+        }
+        $programs = $programRepository->findBy(
+            ['category' => $category],
+            ['id' => 'DESC'],
+            3
+        );
+        return $this->render('category/show.html.twig', [
+            'category' => $category,
+            'programs' => $programs,
+        ]);
+    }
+    }
