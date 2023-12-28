a13f1bb5cb96065ae7bd39b911e2b3d52859719a
Pagot Marion <marion.pagot@gmail.com>
2023-12-28 18:45:31 +0100

    connexion utilisateurs quete23

--- migrations/Version20231228152301.php
+++ migrations/Version20231228152301.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231222093317 extends AbstractMigration
+final class Version20231228152301 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,14 @@ final class Version20231222093317 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE actor ADD image VARCHAR(255) DEFAULT NULL, ADD updated_at DATETIME DEFAULT NULL');
-        $this->addSql('ALTER TABLE episode DROP slug');
+        $this->addSql('ALTER TABLE user CHANGE email email VARCHAR(180) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON user (email)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE actor DROP image, DROP updated_at');
-        $this->addSql('ALTER TABLE episode ADD slug VARCHAR(255) NOT NULL');
+        $this->addSql('DROP INDEX UNIQ_8D93D649E7927C74 ON `user`');
+        $this->addSql('ALTER TABLE `user` CHANGE email email VARCHAR(50) NOT NULL');
     }
 }
